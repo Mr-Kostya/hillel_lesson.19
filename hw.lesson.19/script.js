b80dbaf8a0ddf79cc68fb6cdab8280e1cfae7d8f
Mr-Kostya <bihon.kos@gmail.com>
2021-10-02 16:14:27 +0300

    111

--- hw.lesson.19/script.js
+++ hw.lesson.19/script.js
@@ -1,10 +1,11 @@
 class Tabs {
     #tabsEl;
 
-    static ITEM_C = 'tabs_main';
-    static TITLE_C = 'tabs_title';
-    static BODY_C = 'tabs_body';
-    static ACTIVE_C = 'active';
+    static ITEM_C = "tabs_main";
+    static TITLE_C = "tabs_title";
+    static BODY_C = "tabs_body";
+    static ACTIVE_C = "active";
+    static ACTIVE_TAB_C = "tab-active";
 
     constructor(tabsEl) {
         this.#tabsEl = tabsEl;
@@ -12,9 +13,19 @@ class Tabs {
         this.bindEvents();
     }
 
+    activateTab(element) {
+        const tabs = document.getElementsByClassName(Tabs.TITLE_C);
+        Array.prototype.forEach.call(tabs, (tab) => {
+            console.log(tab.classList);
+            tab.classList.remove(Tabs.ACTIVE_TAB_C);
+        });
+        element.classList.add(Tabs.ACTIVE_TAB_C);
+    }
+
     onTabsElClick(e) {
         if (e.target.classList.contains(Tabs.TITLE_C)) {
             const headerEl = e.target;
+            this.activateTab(headerEl);
             const contentEl = this.openContent(headerEl);
             const activeEl = this.findContent();
 
@@ -26,27 +37,27 @@ class Tabs {
         }
     }
 
-    toggleContent (el) {
+    toggleContent(el) {
         el.classList.toggle(Tabs.ACTIVE_C);
     }
 
-    openContent (el) {
-        const itemEl = el.closest('.' + Tabs.ITEM_C);
-        return itemEl.querySelector('.' + Tabs.BODY_C);
+    openContent(el) {
+        const itemEl = el.closest("." + Tabs.ITEM_C);
+        return itemEl.querySelector("." + Tabs.BODY_C);
     }
 
-    findContent () {
-        return this.#tabsEl.querySelector('.' + Tabs.ACTIVE_C);
-    }
-
-    closeContent (el) {
+    closeContent(el) {
         el.classList.remove(Tabs.ACTIVE_C);
     }
 
-    bindStyles(){
-        const tabsItems =  this.#tabsEl.children;
+    findContent() {
+        return this.#tabsEl.querySelector("." + Tabs.ACTIVE_C);
+    }
 
-        for(let itemEl of tabsItems) {
+    bindStyles() {
+        const tabsItems = this.#tabsEl.children;
+
+        for (let itemEl of tabsItems) {
             const [headerEl, contentEl] = itemEl.children;
 
             itemEl.classList.add(Tabs.ITEM_C);
@@ -55,15 +66,12 @@ class Tabs {
         }
     }
 
-    bindEvents(){
-        this.#tabsEl.addEventListener('click', (e) => this.onTabsElClick(e));
+    bindEvents() {
+        this.#tabsEl.addEventListener("click", (e) => this.onTabsElClick(e));
     }
 }
 
-const tabsEl = document.querySelector('#tabs');
-new Tabs(container);
-
-
-
+const tabsEl = document.querySelector("#container");
+new Tabs(tabsEl);
 
 
